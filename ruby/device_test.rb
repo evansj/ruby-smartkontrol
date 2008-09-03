9786c6dab4d1bfd42008966bc97ce6adafc2dc93
Jon Evans <jon@springyweb.com>
2008-09-03 21:34:16 +0100

    re-arranged the test code

--- ruby/device_test.rb
+++ ruby/device_test.rb
@@ -1,41 +1,17 @@
 #!/usr/bin/env ruby
 
 require 'test/unit'
+require 'test/unit/testsuite'
+require 'test/unit/ui/console/testrunner'
 require 'controller'
 require 'device'
 
 # uses Ruby-SerialPort from http://ruby-serialport.rubyforge.org
 
-class DeviceTestOptions
-  def self.port=(port)
-    @@port = port
-  end
-  def self.port
-    @@port
-  end
-  def self.device_id=(device_id)
-    @@device_id=device_id
-  end
-  def self.device_id
-    @@device_id
-  end
-end
-
-if $0 == __FILE__
-  if ARGV.length != 2
-    puts "Usage: ruby device_test.rb device_id serialport\n\ne.g. ruby device_test.rb S1 /dev/tty.usbserial-A5001pJU"
-    exit 1
-  else
-    puts "Testing device #{ARGV[0]} with serial port #{ARGV[1]}"
-    DeviceTestOptions.device_id = ARGV[0]
-    DeviceTestOptions.port = ARGV[1]
-  end
-end
-
 class DeviceTest < Test::Unit::TestCase
   def setup
-    @controller = Controller.new(DeviceTestOptions.port)
-    @device = Device.new(DeviceTestOptions.device_id)
+    @controller = Controller.new(@@port)
+    @device = Device.new(@@device_id)
     @controller.add_device @device
   end
   
@@ -68,4 +44,32 @@ class DeviceTest < Test::Unit::TestCase
     puts "\nturning LED off"
     @device.led = false
   end
+
+
+  def self.port=(port)
+    @@port = port
+  end
+  def self.device_id=(device_id)
+    @@device_id = device_id
+  end
 end
+
+if $0 == __FILE__
+  if ARGV.length != 2
+    puts "Usage: ruby device_test.rb device_id serialport\n\ne.g. ruby device_test.rb S1 /dev/tty.usbserial-A5001pJU"
+    exit 1
+  else
+    puts "Testing device #{ARGV[0]} with serial port #{ARGV[1]}"
+    DeviceTest.device_id = ARGV[0]
+    DeviceTest.port = ARGV[1]
+  end
+end
+
+class DeviceTests
+  def self.suite
+    suite = Test::Unit::TestSuite.new "Device tests"
+    suite << DeviceTest.suite
+    return suite
+  end
+end
+Test::Unit::UI::Console::TestRunner.run(DeviceTests)
