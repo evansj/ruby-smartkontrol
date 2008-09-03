e5e248f8e85f55a0162f8295a1918d208463db4f
Jon Evans <jon@springyweb.com>
2008-09-03 21:14:04 +0100

    turn device_test.rb into a Test::Unit testcase

--- ruby/device_test.rb
+++ ruby/device_test.rb
@@ -1,42 +1,71 @@
 #!/usr/bin/env ruby
 
+require 'test/unit'
 require 'controller'
 require 'device'
 
 # uses Ruby-SerialPort from http://ruby-serialport.rubyforge.org
 
-port = "/dev/tty.usbserial-A5001pJU"
-
-puts "connecting to serial port #{port}"
-controller = Controller.new(port)
-device = Device.new("S1")
-controller.add_device device
-while true do
-  puts "\npinging #{device.id}"
-  device.ping
-  sleep 0.2
-
-  puts "\ngetting capabilities"
-  device.get_capabilities
-  sleep 0.2
-  
-  puts "\nturning relay on"
-  device.relay = true
-  sleep 0.2
-  
-  puts "\nturning relay off"
-  device.relay = false
-  sleep 0.2
-
-  puts "\nturning LED on"
-  device.led = true
-  sleep 0.2
-  
-  puts "\nturning LED off"
-  device.led = false
-  sleep 0.2
+class DeviceTestOptions
+  def self.port=(port)
+    @@port = port
+  end
+  def self.port
+    @@port
+  end
+  def self.device_id=(device_id)
+    @@device_id=device_id
+  end
+  def self.device_id
+    @@device_id
+  end
 end
 
-controller.close
+if $0 == __FILE__
+  if ARGV.length != 2
+    puts "Usage: ruby device_test.rb device_id serialport\n\ne.g. ruby device_test.rb S1 /dev/tty.usbserial-A5001pJU"
+    exit 1
+  else
+    puts "Testing device #{ARGV[0]} with serial port #{ARGV[1]}"
+    DeviceTestOptions.device_id = ARGV[0]
+    DeviceTestOptions.port = ARGV[1]
+  end
+end
 
+class DeviceTest < Test::Unit::TestCase
+  def setup
+    @controller = Controller.new(DeviceTestOptions.port)
+    @device = Device.new(DeviceTestOptions.device_id)
+    @controller.add_device @device
+  end
+  
+  def teardown
+    @controller.close if @controller
+  end
+  
+  def test_ping
+    puts "\npinging #{@device.id}"
+    @device.ping
+  end
+  
+  def test_get_capabilities
+    puts "\ngetting capabilities"
+    @device.get_capabilities
+  end
 
+  def test_relay
+    puts "\nturning relay on"
+    @device.relay = true
+
+    puts "\nturning relay off"
+    @device.relay = false
+  end
+
+  def test_led
+    puts "\nturning LED on"
+    @device.led = true
+
+    puts "\nturning LED off"
+    @device.led = false
+  end
+end
